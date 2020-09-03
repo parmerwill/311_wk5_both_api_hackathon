c8c43131c18671edf2ea664c3c9c702a695108bf
Apple <apple@Fideles-Mac-Pro.attlocal.net>
2020-09-02 20:48:21 -0500

    commit changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 4001
+const bodyParser = require("body-parser");
+const employeesRouter = require('./routes/employees');
+app.use('/employees', employeesRouter)
 
 app.get('/', (req, res) => {
   res.send('Welcome to our API!')
