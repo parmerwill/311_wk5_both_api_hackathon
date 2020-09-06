0fdcddac067b4d08feb862839950f4e394252d48
Apple <apple@Fideles-Mac-Pro.attlocal.net>
2020-09-05 20:02:49 -0500

    commit changes

--- controllers/employees.js
+++ controllers/employees.js
@@ -35,41 +35,7 @@ const getEmployeesByFirstName = (req, res) => {
       if (err) return handleSQLError(res, err)
       return res.json(rows);
     })
-  }
-
-// const createUser = (req, res) => {
-//   // INSERT INTO USERS FIRST AND LAST NAME 
-//   let sql = 'INSERT INTO users (first_Name, last_Name) VALUES (?,?)'
-//   sql = mysql.format(sql, ["bogus","user"])
-  
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json({ newId: results.insertId });
-//   })
-// }
-
-// const updateUserById = (req, res) => {
-//   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-//   let sql = "UPDATE users SET first_name = ?, last_name = ? WHERE id = ?"
-//   sql = mysql.format(sql, ['bogus','user',req.params.id])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.status(204).json();
-//   })
-// }
-
-// const deleteUserByFirstName = (req, res) => {
-//   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-//   let sql = "DELETE FROM users WHERE  first_name  = ?"
-//   // WHAT GOES IN THE BRACKETS
-//   sql = mysql.format(sql, [req.params.first_name])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
-//   })
-// }
+  };
 
 module.exports = { getEmployees, 
                     getEmployeesById, 
